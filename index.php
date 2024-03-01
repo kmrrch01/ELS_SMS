1736c9483509e55678f8badb1ce508df614aadbe
kmrrch01 <161851821+kmrrch01@users.noreply.github.com>
2024-03-01 16:27:12 +0200

    Public Directory

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This is just a note and I can't think of a better place to put it: 
+ * => The ".htaccess" file is a very important file; it is what reroutes our URLs
+ * and gives us access to the Controllers (etc), please do not in any way alter that file.
+ */
+
+require "../private/core/autoload.php";
+
+$app = new App();
