261405996861a510d0e2b66bd69e0cd98a1a1092
kmrrch01 <161851821+kmrrch01@users.noreply.github.com>
2024-05-02 22:04:53 +0000

    Latest commit + all views MVC 03/05/2024

--- private/core/controller.php
+++ private/core/controller.php
@@ -1,23 +1,40 @@
-<?php
+<?php 
 
-// main controller class
-
-
-class Controller 
+/**
+ * main controller class
+ */
+class Controller
 {
+	
+	public function view($view,$data = array())
+	{
+		extract($data);
+		// code...
 
-    public function view($view,$data = array())
-    {
-        extract($data);
-        //code...
+		if(file_exists("../private/views/" . $view . ".view.php"))
+		{
+			require ("../private/views/" . $view . ".view.php");
+		}else{
+			require ("../private/views/404.view.php");
+		}
+	}
 
-        if(file_exists("../private/views/" . $view . ".view.php"))
-        {
-            return file_get_contents("../private/views/" . $view . ".view.php");
-        }
-        else{
-            return file_get_contents("../private/views/404.view.php");
-        }
-    }
+	public function load_model($model)
+	{
 
+		if(file_exists("../private/models/".ucfirst($model).".php"))
+		{
+			require("../private/models/".ucfirst($model).".php");
+			return $model = new $model();
+		}
+		
+		return false;
+	}
+
+	public function redirect($link)
+	{
+
+		header("Location: ". ROOT . "/".trim($link,"/"));
+		die;
+	}
 }
--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/all.min.css">
-    <meta name="keywords" content="HTML, CSS, JavaScript">
-    <meta name="viewport" content= "width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <title>Home</title>
-</head>
-<body>
-<div style="min-width:350px;">
-
-    <div class="container-fluid">
-          <h1><i class="fa fa-plus"></i>This is home</h1>
-    </div>
-
-</div>
-
-</body>
-</html>
+<?php $this->view('includes/header')?>
+<?php $this->view('includes/nav')?>
+	
+	<div class="container-fluid">
+	 
+	</div>
+ 
+<?php $this->view('includes/footer')?>
