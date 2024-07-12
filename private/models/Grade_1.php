b8fa1ecb8ab1849c5c80271506725ad172c351c8
kmrrch01 <161851821+kmrrch01@users.noreply.github.com>
2024-07-12 09:41:44 +0000

    committ

---
+++ private/models/Grade_1.php
@@ -0,0 +1,20 @@
+<?php
+
+class Grade_1 extends Model{
+
+    protected $table = 'grade_1';
+
+    protected $allowedColumns = [
+        'student_id',
+        'class_id',
+        'fullname',
+        'user_id',
+        'english',
+        'arabic',
+        'math',
+        'science',
+        'religion',
+        'english',
+       
+    ];
+}
