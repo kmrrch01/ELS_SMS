6b65bfaa181a44fed7932c7b7d623e0b0bfd6fdd
marwannini <marwanaldaou123@gmail.com>
2024-07-26 14:50:40 +0300

    Add files via upload

---
+++ private/core/config.php
@@ -0,0 +1,10 @@
+<?php
+define('ROOT', 'http://localhost/ELS/public');
+define('ASSETS', 'http://localhost/ELS/public/assets');
+
+
+define('DBNAME', 'els');
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASSWORD', '');
+define('DBDRIVER', 'mysql');
